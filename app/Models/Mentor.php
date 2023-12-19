c9b2c1fc28d2f5dccf45e53959be667b118212e5
itsmee3223 <tamvam.pemberani@gmail.com>
2023-12-19 10:48:17 +0700

    mentor migration & table

---
+++ app/Models/Mentor.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mentor extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'experience',
+    ];
+
+    /**
+     * Mendefinisikan relasi ke model User.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
